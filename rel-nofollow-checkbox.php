609fef7c1039ffcbcf2a0cf8519c154fb194c696
ffsantos92 <ffsantos92@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-27 18:17:38 +0000

    Compatible with WP 4.0.1
    git-svn-id: https://plugins.svn.wordpress.org/rel-nofollow-checkbox/trunk@1034028 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rel-nofollow-checkbox.php
+++ rel-nofollow-checkbox.php
@@ -1,16 +1,16 @@
 <?php
 /**
  * Plugin Name: Rel Nofollow Checkbox
- * Plugin URI:  http://www.fabiosantos.pt/rel-nofollow-checkbox/
- * Description: Adds a checkbox in the insert/edit link popup for including (or not) nofollow attribute in links.
- * Version:     1.0.1
+ * Plugin URI:  https://github.com/ffsantos92/rel-nofollow-checkbox
+ * Description: This plugin adds a simple checkbox in the insert/edit link popup for including nofollow attribute.
+ * Version:     1.0.2
  * Author:      Fabio Santos
  * Author URI:  http://www.fabiosantos.pt
  */
 function overwrite_wplink() {
 	// Disable wplink
 	wp_deregister_script( 'wplink' );
-	
+
 	// Register a new script file to be linked
 	wp_register_script( 'wplink', plugins_url( 'wplink.min.js', __FILE__), array( 'jquery', 'wpdialogs' ), false, 1 );
 }
